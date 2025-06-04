101051b60f46bff0bcd514ec05c98f7c2cab6080
S312714 <fedehope03@gmail.com>
2025-06-04 16:57:52 +0200

    primo salvataggio

--- UI/controller.py
+++ UI/controller.py
@@ -7,11 +7,26 @@ class Controller:
         self._view = view
         # the model, which implements the logic of the program and holds the data
         self._model = model
+        self.store = None
 
+    def fillddStore(self):
+        stores = self._model.getStores()
+        for s in stores:
+            self._view._ddStore.options.append(ft.dropdown.Option(s,on_click=self.setStore))
 
-
+    def setStore(self,e):
+        if e.control.data != "":
+            self.store = e.control.data
+        else:
+            self.store = None
     def handleCreaGrafo(self, e):
-        pass
+        if self.store is None:
+            self._view.txt_result.control.clear()
+            self._view.txt_result.control.append(ft.Text("Non hai selezionato nessun negozio",color="red"))
+            self._view.update_page()
+            return
+
+        self._model.buildGraph(self.store)
     def handleCerca(self, e):
         pass
 
--- database/DAO.py
+++ database/DAO.py
@@ -4,3 +4,36 @@ from database.DB_connect import DBConnect
 class DAO():
     def __init__(self):
         pass
+
+    @staticmethod
+    def getAllStore():
+        conn = DBConnect.get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        result = []
+
+        query = """select s.store_id 
+from stores s """
+        cursor.execute(query)
+        for row in cursor:
+            result.append(row["store_id"])
+        cursor.close()
+        conn.close()
+
+        return result
+
+    @staticmethod
+    def getNodes(id):
+        conn = DBConnect.get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        result = []
+
+        query = """"""
+        cursor.execute(query)
+        for row in cursor:
+            result.append()
+        cursor.close()
+        conn.close()
+
+        return result
